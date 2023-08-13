b60ea125321d5194a2f4131a00551da99b3a4b2c
sancend <salilicen@gmail.com>
2023-08-12 22:19:59 -0500

    Mensaje descriptivo de los cambios

--- pages/3-Canvas.py
+++ pages/3-Canvas.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.subheader("Canvas de machine learning del modelo de predicción de enfermedad del corazón")
 
 
-st.image("images/Canvas.png", caption="Canvas - Sandra Cendales")
+st.image("images/Canvas.png", caption="Canvas - Sandra Cendales A.")
